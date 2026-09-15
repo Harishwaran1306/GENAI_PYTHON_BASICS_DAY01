627b08bd5e8acfe838816bfe46fa43427aeb3717
Harishwaran1306 <harishwaran.mech.23@gmail.com>
2026-09-15 21:35:47 +0530

    Add files via upload

---
+++ type_conversion_Day01.py
@@ -0,0 +1,18 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> A=96
+>>> type(A)
+<class 'int'>
+>>> float(A)
+96.0
+>>> S= str(A)
+>>> print(S)
+96
+>>> type(S)
+<class 'str'>
+>>> A
+96
+>>> S
+'96'
+>>> float(S)
+96.0
