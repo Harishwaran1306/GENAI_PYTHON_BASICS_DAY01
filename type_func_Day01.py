7d4a0e03b2537db6f3d2c8c337b71ee8cc190691
Harishwaran1306 <harishwaran.mech.23@gmail.com>
2026-09-15 21:32:45 +0530

    Add files via upload

---
+++ type_func_Day01.py
@@ -0,0 +1,10 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> Name='Charles'
+>>> A=Name
+>>> print(A)
+Charles
+>>> type(A)
+<class 'str'>
+>>> type(Name)
+<class 'str'>
